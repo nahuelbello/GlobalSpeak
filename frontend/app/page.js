83b9b51286f0d3816f88b71a6d66b76bf184af0e
Nahuel Bello <nahuel.bellorendon@gmail.com>
2025-05-30 15:26:19 +0200

    commit de prueba

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-primary">¡Hola, GlobalSpeak!</h1>
+      <h2 className="text-4xl font-bold text-primary">Esto es un test</h2>
     </main>
   )
 }
